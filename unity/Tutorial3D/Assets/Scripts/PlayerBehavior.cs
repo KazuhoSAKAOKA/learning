60b34f5f6d7f2c6ab5313f0ff53c12c277918bb6
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-06-05 18:00:54 +0900

    unity

---
+++ unity/Tutorial3D/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 75.0f;
+
+    private float vInput;
+    private float hInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+}
